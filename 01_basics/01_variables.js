e9b031f0691d6b9c9a63e9885b3c4a48cbff27a1
kailash.dattob <50129376+kailash-dattob@users.noreply.github.com>
2025-03-16 03:32:05 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "kailash@google.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/* Prefer not to use var because of issue in block scope and functional scope */
+
