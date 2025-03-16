3d2dc8d2e6a28ddec8057fda05d27c7f8cf413de
kailash.dattob <50129376+kailash-dattob@users.noreply.github.com>
2025-03-16 03:31:03 +0000

    Data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all js code as newer version
+//alert() we are using nodejs, not browser
+
+//Number => 21515
+//Bigint => 
+// String =>    ""
+//Boolean => true/false
+//null => standalone value
+//undefined =>
+//Symbol => unique
+
+let age = 18
+
+
+//Object 
+
+console.log(typeof age)
+console.log(typeof null)
