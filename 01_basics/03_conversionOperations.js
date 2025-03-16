4c84818d350638f438d0bc36f6cabbc29d8f3988
kailash.dattob <50129376+kailash-dattob@users.noreply.github.com>
2025-03-16 17:11:18 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -27,3 +27,32 @@ let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
 
+
+//****************** Operations***************** */
+let value = 3
+let negValue = -value
+//console.log(negValue)
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/3)
+console.log(2%3)
+
+let str1 = "hello"
+let str2 = " kailash"
+let str3 = str1 + str2
+
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 +2)
+console.log(1 + 2 + "2")
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
+//link to study https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
