831f1d8db0a6fd2b2b287cc4afdb9c28217ccd89
kailash.dattob <50129376+kailash-dattob@users.noreply.github.com>
2025-03-18 02:39:30 +0000

    Datatye Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+//premitive
+//7 types : String, Number, boolean, null, undefined, Symbol, Bigint
+//string
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 3481684649845561616n
+
+//refrence type||(Nonpremitive)
+
+//Array, Objects, Functions
+
+const heros = ['Shaktiman', 'naagraj','doga']
+
+let myObj = {
+    name : "kailash",
+    age : 40,
+}
+
+const myFunction = function(){
+
+    console.log("hello world")
+
+}
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
